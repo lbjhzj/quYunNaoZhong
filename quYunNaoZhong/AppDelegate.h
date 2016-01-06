4b3522b8af9039cceb9aa9b284f13a8d7b9b1e4c
趣云科技 <bryanthao@sina.com>
2016-01-06 13:10:34 +0800

    Initial Commit

---
+++ quYunNaoZhong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  quYunNaoZhong
+//
+//  Created by 趣云科技 on 16/1/6.
+//  Copyright © 2016年 趣云科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ quYunNaoZhong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  quYunNaoZhong
+//
+//  Created by 趣云科技 on 16/1/6.
+//  Copyright © 2016年 趣云科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
